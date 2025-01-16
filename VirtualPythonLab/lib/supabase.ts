e78178b228f6f2ac1f929927ec12b1aa6f9271c8
Dzulfaqor Ali Dipangegara <whysoserius30@gmail.com>
2025-01-16 13:15:07 +0700

    Revert "Merge branch 'main' of https://github.com/dzulfaqorali196/Mobille-Dev-PAWM"
    This reverts commit 0ed1ad72053fda4e3079aeff9484228eb1bf3aeb, reversing
    changes made to c98884a35fa4e6f9a6c2c633b3969466912306e3.

--- VirtualPythonLab/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import 'react-native-url-polyfill/auto';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://yoxeghgqwyhavtlzsrjb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlveGVnaGdxd3loYXZ0bHpzcmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NjUwNDcsImV4cCI6MjA0NTU0MTA0N30.NegtXsNzySa62SQOEM4lWlDhIpTUxf_R-_ai4g8hJk8';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
